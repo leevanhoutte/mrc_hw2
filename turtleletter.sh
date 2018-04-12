887dc4fd2535cc063e37ea6fb4ce5733f47acfb3
leevanhoutte <37637358+leevanhoutte@users.noreply.github.com>
2018-04-12 12:13:26 -0700

    Delete turtleletter.sh

--- turtleletter.sh
+++
@@ -1,5 +0,0 @@
-rosservice call /turtle1/teleport_absolute 2.5 8.5 4.712 
-rosservice call /clear
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0, 0.0]' '[0.0, 0.0, 1.57]'
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[3.0, 0, 0.0]' '[0.0, 0.0, 0.0]'
